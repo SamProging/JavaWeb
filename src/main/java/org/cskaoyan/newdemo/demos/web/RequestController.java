22c0dc717dd62387326e136f3f35251a5738f116
SamCoding <h2ostar@outlook.com>
2026-08-11 16:53:32 +0800

    Initial commit

---
+++ src/main/java/org/cskaoyan/newdemo/demos/web/RequestController.java
@@ -0,0 +1,11 @@
+package org.cskaoyan.newdemo.demos.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class RequestController {
+
+    @RequestMapping("/request")
+    public String Request(){
+
+    }
+}
---
+++ src/test/java/org/cskaoyan/newdemo/NewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cskaoyan.newdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
